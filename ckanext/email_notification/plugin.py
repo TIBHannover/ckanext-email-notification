07d8cf4c253cbb31cd35e4a93b51adef7dbd5992
pooya.oa <pooya.oladazimi@tib.eu>
2021-07-27 13:11:06 +0200

    do not send email when there is no new user

--- ckanext/email_notification/plugin.py
+++ ckanext/email_notification/plugin.py
@@ -9,6 +9,9 @@ TIME_DELTA = 120
 
 def send_email_notification():
     new_users = Helper.get_new_users(TIME_DELTA)
+    if len(new_users) == 0:
+        return None
+        
     sys_admins = Helper.get_sysadmins_email()
     subject = "New CKAN User"
     body = Helper.create_email_body(new_users)
